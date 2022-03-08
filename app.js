bfc5586f0ec2dd34c5f07fc1e9f313081d5784be
Gulam Anas <g.anas786@gmail.com>
2022-03-08 15:08:42 +0530

    some changes

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 // Responsive Navigation
 const navSlide = document.querySelector(".burger");
 navSlide.addEventListener("click", () => {
-    const nav = document.querySelector(".navlist");
-    
-    navSlide.classList.toggle("switch");
-    nav.classList.toggle("responsiveNav");
+  const nav = document.querySelector(".navlist");
+
+  navSlide.classList.toggle("switch");
+  nav.classList.toggle("responsiveNav");
+});
+
+// AOS Animation
+AOS.init({
+  offset: 500,
+  duration: 1000,
+  once: true,
 });
