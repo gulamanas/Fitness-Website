02e22696a429ec6bcbde71edfda0928491117914
Gulam Anas <g.anas786@gmail.com>
2022-03-27 23:02:44 +0530

    date added conditionally

--- app.js
+++ app.js
@@ -7,6 +7,51 @@ navSlide.addEventListener("click", () => {
   nav.classList.toggle("responsiveNav");
 });
 
+// Get Current Month
+const month = document.getElementById("month");
+const monthValues = document.getElementById("month_value");
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const d = new Date();
+
+const monthsInNumber = months[d.getMonth()];
+monthValues.innerHTML = monthsInNumber.toUpperCase();
+month.innerHTML = d.getMonth() + 1;
+
+// Get date
+function lastDate() {
+  let newDate = document.getElementById("date");
+  if (monthsInNumber == months[1]) {
+    newDate.innerHTML = 28;
+  } else if (
+    monthsInNumber == months[3] ||
+    months[5] ||
+    months[8] ||
+    months[10]
+  ) {
+    newDate.innerHTML = 30;
+  } else {
+    newDate.innerHTML = 31;
+  }
+}
+lastDate();
+
+// Get Year
+const year = document.getElementById("year");
+year.innerHTML = d.getFullYear();
+
 // AOS Animation
 AOS.init({
   offset: 500,
